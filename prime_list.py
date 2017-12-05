6b9ad4c5172528160f60bc50c5fa22ab1948da5f
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-05 21:24:01 +0100

    Minor error

--- prime_list.py
+++ prime_list.py
@@ -1,7 +1,12 @@
+import os
+
 # Method to generate small primes in a list.
 def genPrimes():
     primes = []
     for i in range(2,1299827):
+        if(i%2500==0):
+            os.system("clear")
+            print(str(int((i/1299827)*100))+"% completado")
         prime=True
         for j in range(2,int(i/2+1)):
             if i%j==0:
@@ -11,7 +16,7 @@ def genPrimes():
             primes.append(i)
     f = open("prime_list.txt", "w")
     for i in range(len(primes)-2):
-        f.write(str(prime_item) + ",")
+        f.write(str(primes[i]) + ",")
     f.write(str(primes[-1]))
 
 # Obtain prime list from txt.
