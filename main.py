43a664bc764a742e8a86e1e75e69109de7bcb306
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-06 12:44:57 +0100

    Threading support added

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import primes
 import time
 import prime_list
+import threading
+
+THREAD_NUMBER = 50
+lock = threading.Lock()
+prime_array = []
+threads = []
+
+def checkPrime(exponent,factors):
+    if primes.isPrimeLL(int(exponent),factors):
+        with lock:
+            prime_array.append(primes.mersennePrime(exponent))
 
 def main():
-    prime_array = []
     factors = prime_list.readPrimes()
     good_factors = prime_list.checkModulus(factors)
-    for i in range(1,3500):
+    for i in range(1,3500,THREAD_NUMBER):
         print(str(i))
-        if primes.isPrimeLL(int(i),good_factors):
-            prime_array.append(primes.mersennePrime(i))
+        threads = []
+        for j in range(THREAD_NUMBER):
+            threads.append(threading.Thread(target = checkPrime, args = (i+j,good_factors)))
+        for j in range(THREAD_NUMBER):
+            threads[j].start()
+        for j in range(THREAD_NUMBER):
+            threads[j].join()
     for prime in prime_array:
         print(str(prime) + " is a Mersenne prime.\n")
 
