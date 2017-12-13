a3ec4fd0c051167abf197971dd1ac55e5c3ce9c0
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2017-12-14 00:01:43 +0100

    Divisors function

---
+++ properties.py
@@ -0,0 +1,14 @@
+# Module to test properties of the Mersenne primes obtained.
+
+################################################################################
+##                         GENERAL PROPERTIES FUNCTIONS                       ##
+################################################################################
+
+# Returns a list with the divisors of a given number.
+def divisors(number):
+    div = []
+    i = 0
+    while i < number/2:
+        if number%i==0:
+            div.append(i)
+    return div
